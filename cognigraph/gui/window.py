8e5722344d5beedade849c2a80d2c719a730f014
andreasxp <andres.zhukov@gmail.com>
2018-05-06 17:06:54 +0300

    Add start/stop button
    Former-commit-id: 02e2b3f7ee186750cc20e991055ab5cef2e674e9

--- cognigraph/gui/window.py
+++ cognigraph/gui/window.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pyqtgraph import QtGui
+from pyqtgraph import QtCore, QtGui
 
 from ..pipeline import Pipeline
 from .controls import Controls
@@ -12,7 +12,12 @@ class GUIWindow(QtGui.QMainWindow):
         self._pipeline = pipeline  # type: Pipeline
         self._controls = Controls(pipeline=self._pipeline)
         self._controls_widget = self._controls.widget
-
+        
+        #Start button and timer
+        self.timer = QtCore.QTimer()
+        self._control_button = QtGui.QPushButton("Start")
+        self._control_button.clicked.connect(self._toggle_timer)
+        
         self.main_layout = None  # type: QtGui.QBoxLayout
         
     def init_ui(self):
@@ -20,10 +25,16 @@ class GUIWindow(QtGui.QMainWindow):
 
         central_widget = QtGui.QWidget()
         self.setCentralWidget(central_widget)
-
+        
+        # Build the controls portion of the window
+        controls_layout = QtGui.QVBoxLayout()
+        controls_layout.addWidget(self._controls_widget)
+        controls_layout.addWidget(self._control_button)        
+        
+        # Add control portion to the main layout
         main_layout = QtGui.QHBoxLayout()
         self._controls_widget.setMinimumWidth(400)
-        main_layout.addWidget(self._controls_widget)
+        main_layout.addLayout(controls_layout)
 
         self.centralWidget().setLayout(main_layout)
         self.main_layout = main_layout
@@ -33,9 +44,17 @@ class GUIWindow(QtGui.QMainWindow):
         for node_widget in self._node_widgets:
             node_widget.setMinimumWidth(400)
             
-            #insert widget at before-the-end pos (just before controls widget)
+            # insert widget at before-the-end pos (just before controls widget)
             self.main_layout.insertWidget(self.main_layout.count()-1, node_widget)
 
+    def _toggle_timer(self):
+        if self.timer.isActive():
+            self.timer.stop()
+            self._control_button.setText("Start")
+        else:
+            self.timer.start()
+            self._control_button.setText("Pause")            
+            
     @property
     def _node_widgets(self) -> List[QtGui.QWidget]:
         node_widgets = list()
