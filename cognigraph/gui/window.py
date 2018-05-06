6c9a613183ca15e382ee4c5dc2f595858150d6a0
andreasxp <andres.zhukov@gmail.com>
2018-05-06 14:54:57 +0300

    Refactor GUIWindow
    Former-commit-id: f041ffb91cee00f637e2373dd26049339b9be166

--- cognigraph/gui/window.py
+++ cognigraph/gui/window.py
@@ -14,29 +14,27 @@ class GUIWindow(QtGui.QMainWindow):
         self._controls_widget = self._controls.widget
 
         self.main_layout = None  # type: QtGui.QBoxLayout
-        self.widgets_layout = None  # type: QtGui.QBoxLayout
-
+        
     def init_ui(self):
         self._controls.initialize()
 
         central_widget = QtGui.QWidget()
         self.setCentralWidget(central_widget)
 
-        widgets_layout = QtGui.QHBoxLayout()
         main_layout = QtGui.QHBoxLayout()
-        main_layout.addLayout(widgets_layout)
         self._controls_widget.setMinimumWidth(400)
         main_layout.addWidget(self._controls_widget)
 
         self.centralWidget().setLayout(main_layout)
         self.main_layout = main_layout
-        self.widgets_layout = widgets_layout
 
     def initialize(self):
         self._pipeline.initialize_all_nodes()
         for node_widget in self._node_widgets:
             node_widget.setMinimumWidth(400)
-            self.widgets_layout.addWidget(node_widget)
+            
+            #insert widget at before-the-end pos (just before controls widget)
+            self.main_layout.insertWidget(self.main_layout.count()-1, node_widget)
 
     @property
     def _node_widgets(self) -> List[QtGui.QWidget]:
