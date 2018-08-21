c90b23b5bd0bbea5ddeeadeb2e0ed84ac289ca9a
andreasxp <andres.zhukov@gmail.com>
2018-08-22 02:14:23 +0300

    Add a gif recorder; fix trailing spaces;
    Former-commit-id: 27e89bed65c0e298dbae03433a651abbdb374281

--- scripts/launch_test.py
+++ scripts/launch_test.py
@@ -1,7 +1,6 @@
 ﻿import sys
 import time
 
-sys.path.append('../vendor/nfb') # For nfb submodule
 
 from PyQt5 import QtCore, QtGui
 import mne
@@ -13,13 +12,20 @@ from cognigraph.nodes import sources, processors, outputs, node
 from cognigraph import TIME_AXIS
 from cognigraph.gui.window import GUIWindow
 
+# Убираем предупреждения numpy, иначе в iPython некрасиво как-то Ж)
+import numpy as np
+np.warnings.filterwarnings('ignore')
+
+sys.path.append('../vendor/nfb')  # For nfb submodule
+
 app = QtGui.QApplication(sys.argv)
 
 # Собираем узлы в пайплайн
 
 pipeline = Pipeline()
 
-launch_test_filepath = QtGui.QFileDialog.getOpenFileName(caption="Select Data", filter="Brainvision (*.eeg *.vhdr *.vmrk)")[0]
+launch_test_filepath = QtGui.QFileDialog.getOpenFileName(
+    caption="Select Data", filter="Brainvision (*.eeg *.vhdr *.vmrk)")[0]
 source = sources.BrainvisionSource(file_path=launch_test_filepath)
 source.loop_the_file = True
 source.MAX_SAMPLES_IN_CHUNK = 10000
@@ -41,7 +47,8 @@ pipeline.add_processor(envelope_extractor)
 
 # Outputs
 global_mode = outputs.ThreeDeeBrain.LIMITS_MODES.GLOBAL
-three_dee_brain = outputs.ThreeDeeBrain(limits_mode=global_mode, buffer_length=6)
+three_dee_brain = outputs.ThreeDeeBrain(limits_mode=global_mode,
+                                        buffer_length=6)
 pipeline.add_output(three_dee_brain)
 # pipeline.add_output(outputs.LSLStreamOutput())
 
@@ -54,7 +61,7 @@ pipeline.add_output(signal_viewer, input_node=linear_filter)
 window = GUIWindow(pipeline=pipeline)
 window.init_ui()
 window.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
-#window.show() # Will show after init
+# window.show() # Will show after init
 
 
 # Инициализируем все узлы
@@ -65,11 +72,13 @@ window.initialize()
 
 # Set bad channels and calculate interpolation matrix manually
 bad_channel_labels = ['Fp2', 'F5', 'C5', 'F2', 'PPO10h', 'POO1', 'FCC2h']
-preprocessing._bad_channel_indices = mne.pick_channels(source.mne_info['ch_names'], include=bad_channel_labels)
+preprocessing._bad_channel_indices = mne.pick_channels(
+    source.mne_info['ch_names'], include=bad_channel_labels)
 preprocessing._samples_to_be_collected = 0
 preprocessing._enough_collected = True
 
-preprocessing._interpolation_matrix = preprocessing._calculate_interpolation_matrix()
+preprocessing._interpolation_matrix = (
+    preprocessing._calculate_interpolation_matrix())
 message = node.Message(there_has_been_a_change=True,
                        output_history_is_no_longer_valid=True)
 preprocessing._deliver_a_message_to_receivers(message)
@@ -79,28 +88,29 @@ preprocessing._deliver_a_message_to_receivers(message)
 vhdr_file_path = os.path.splitext(source.file_path)[0] + '.vhdr'
 start_s, stop_s = 80, 100
 with source.not_triggering_reset():
-    source.data, _ = read_brain_vision_data(vhdr_file_path, time_axis=TIME_AXIS, start_s=start_s, stop_s=stop_s)
+    source.data, _ = read_brain_vision_data(
+        vhdr_file_path, time_axis=TIME_AXIS, start_s=start_s, stop_s=stop_s)
 
 # Подключаем таймер окна к обновлению пайплайна
 class AsyncUpdater(QtCore.QRunnable):
     _stop_flag = False
-    
+
     def __init__(self):
         super(AsyncUpdater, self).__init__()
         self.setAutoDelete(False)
 
     def run(self):
         self._stop_flag = False
-        
+
         while self._stop_flag == False:
             start = time.time()
             pipeline.update_all_nodes()
             end = time.time()
-            
+
             # Force sleep to update at 10Hz
             if end - start < 0.1:
                 time.sleep(0.1 - (end - start))
-        
+
     def stop(self):
         self._stop_flag = True
 
@@ -112,7 +122,7 @@ def toggle_updater():
     global pool
     global updater
     global is_paused
-    
+
     if is_paused == True:
         is_paused = False
         pool.start(updater)
@@ -120,12 +130,10 @@ def toggle_updater():
         is_paused = True
         updater.stop()
         pool.waitForDone()
-        
-window.control_button.clicked.connect(toggle_updater)
 
-# Убираем предупреждения numpy, иначе в iPython некрасиво как-то Ж)
-import numpy as np
-np.warnings.filterwarnings('ignore')
+
+window.run_button.clicked.connect(toggle_updater)
+
 
 # Show window and exit on close
 window.show()
