c33365e2cd14af72298a9e6c627393c1ca03bc19
dmalt <dm.altukhov@ya.ru>
2018-10-08 11:16:23 +0300

    Optimize beamformer performance
    - speed up beamformer loop with jit;
    - search and destroy slow lines in make_lcmv
    
    
    Former-commit-id: 268dc4fda9566cf866eba8a4f2eaae89e63dadbe

--- cognigraph/helpers/make_lcmv.py
+++ cognigraph/helpers/make_lcmv.py
@@ -1,6 +1,6 @@
 import numpy as np
 from copy import deepcopy
-from scipy import linalg
+from numpy import linalg  # works faster than scipy on anaconda
 
 from mne.io.pick import (pick_types, pick_channels_forward, pick_channels_cov, pick_info)
 from mne.cov import compute_whitener
@@ -79,7 +79,8 @@ def _prepare_beamformer_input(info, forward, label, picks, pick_ori):
     fwd_ch_names = forward['sol']['row_names']
     # Keep channels in forward present in info:
     fwd_ch_names = [ch for ch in fwd_ch_names if ch in info_ch_names]
-    forward = pick_channels_forward(forward, fwd_ch_names, verbose='ERROR')
+    # This line takes ~48 milliseconds on kernprof
+    # forward = pick_channels_forward(forward, fwd_ch_names, verbose='ERROR')
     picks_forward = [fwd_ch_names.index(ch) for ch in ch_names]
 
     # Get gain matrix (forward operator)
@@ -224,7 +225,8 @@ def make_lcmv(info, forward, data_cov, reg=0.05, noise_cov=None, label=None,
     Cm = data_cov['data']
 
     # check number of sensor types present in the data
-    _check_one_ch_type(info, picks, noise_cov)
+    # This line takes ~23 ms on kernprof
+    # _check_one_ch_type(info, picks, noise_cov)
 
     # apply SSPs
     is_ssp = False
@@ -275,104 +277,11 @@ def make_lcmv(info, forward, data_cov, reg=0.05, noise_cov=None, label=None,
         if not isinstance(reduce_rank, bool):
             raise ValueError('reduce_rank has to be True or False '
                              ' (got %s).' % reduce_rank)
-    
+
     # Compute spatial filters
     W = np.dot(G.T, Cm_inv)
-    W, is_free_ori = beam_loop(W, G, Cm_inv_sq, Cm_inv, is_free_ori, pick_ori, weight_norm, reduce_rank, noise)
-    # n_orient = 3 if is_free_ori else 1
-    # n_sources = G.shape[1] // n_orient
-    # for k in range(n_sources):
-    #     Wk = W[n_orient * k: n_orient * k + n_orient]
-    #     Gk = G[:, n_orient * k: n_orient * k + n_orient]
-    #     if np.all(Gk == 0.):
-    #         continue
-    #     Ck = np.dot(Wk, Gk)
-
-    #     # Compute scalar beamformer by finding the source orientation which
-    #     # maximizes output source power
-    #     if pick_ori == 'max-power':
-    #         # weight normalization and orientation selection:
-    #         if weight_norm is not None and pick_ori == 'max-power':
-    #             # finding optimal orientation for NAI and unit-noise-gain
-    #             # based on [2]_, Eq. 4.47
-    #             tmp = np.dot(Gk.T, np.dot(Cm_inv_sq, Gk))
-
-    #             if reduce_rank:
-    #                 # use pseudo inverse computation setting smallest component
-    #                 # to zero if the leadfield is not full rank
-    #                 tmp_inv = _eig_inv(tmp, tmp.shape[0] - 1)
-    #             else:
-    #                 # use straight inverse with full rank leadfield
-    #                 try:
-    #                     tmp_inv = linalg.inv(tmp)
-    #                 except np.linalg.linalg.LinAlgError:
-    #                     raise ValueError('Singular matrix detected when '
-    #                                      'estimating LCMV filters. Consider '
-    #                                      'reducing the rank of the leadfield '
-    #                                      'by using reduce_rank=True.')
-
-    #             eig_vals, eig_vecs = linalg.eig(np.dot(tmp_inv,
-    #                                                    np.dot(Wk, Gk)))
-
-    #             if np.iscomplex(eig_vecs).any():
-    #                 raise ValueError('The eigenspectrum of the leadfield at '
-    #                                  'this voxel is complex. Consider '
-    #                                  'reducing the rank of the leadfield by '
-    #                                  'using reduce_rank=True.')
-
-    #             idx_max = eig_vals.argmax()
-    #             max_ori = eig_vecs[:, idx_max]
-    #             Wk[:] = np.dot(max_ori, Wk)
-    #             Gk = np.dot(Gk, max_ori)
-
-    #             # compute spatial filter for NAI or unit-noise-gain
-    #             tmp = np.dot(Gk.T, np.dot(Cm_inv_sq, Gk))
-    #             denom = np.sqrt(tmp)
-    #             Wk /= denom
-    #             if weight_norm == 'nai':
-    #                 Wk /= np.sqrt(noise)
-
-    #             is_free_ori = False
-
-    #         # no weight-normalization and max-power is not implemented yet:
-    #         else:
-    #             raise NotImplementedError('The max-power orientation '
-    #                                       'selection is not yet implemented '
-    #                                       'with weight_norm set to None.')
-
-    #     else:  # do vector beamformer
-    #         # compute the filters:
-    #         if is_free_ori:
-    #             # Free source orientation
-    #             Wk[:] = np.dot(linalg.pinv(Ck, 0.1), Wk)
-    #         else:
-    #             # Fixed source orientation
-    #             Wk /= Ck
-
-    #         # handle noise normalization with free/normal source orientation:
-    #         if weight_norm == 'nai':
-    #             raise NotImplementedError('Weight normalization with neural '
-    #                                       'activity index is not implemented '
-    #                                       'yet with free or fixed '
-    #                                       'orientation.')
-
-    #         if weight_norm == 'unit-noise-gain':
-    #             noise_norm = np.sum(Wk ** 2, axis=1)
-    #             if is_free_ori:
-    #                 noise_norm = np.sum(noise_norm)
-    #             noise_norm = np.sqrt(noise_norm)
-    #             if noise_norm == 0.:
-    #                 noise_norm_inv = 0  # avoid division by 0
-    #             else:
-    #                 noise_norm_inv = 1. / noise_norm
-    #             Wk[:] *= noise_norm_inv
-
-    # Pick source orientation maximizing output source power
-    # if pick_ori == 'max-power':
-    #     W = W[0::3]
-    # elif pick_ori == 'normal':
-    #     W = W[2::3]
-    #     is_free_ori = False
+    W, is_free_ori = beam_loop(W, G, Cm_inv_sq, Cm_inv, is_free_ori,
+                               pick_ori, weight_norm, reduce_rank, noise)
 
     filters = dict(weights=W, data_cov=data_cov, noise_cov=noise_cov,
                    whitener=whitener, weight_norm=weight_norm,
