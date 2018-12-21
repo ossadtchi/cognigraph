bf8e1a8cb4b25a01595329cd7a75657d467a42bd
dmalt <dm.altukhov@ya.ru>
2018-12-21 18:24:52 +0300

    temp

--- cognigraph/nodes/processors.py
+++ cognigraph/nodes/processors.py
@@ -988,35 +988,6 @@ class AmplitudeEnvelopeCorrelations(ProcessorNode):
             factor=self.factor, column_count=channel_count)
         self._envelope_extractor.apply = pynfb_ndarray_function_wrapper(
             self._envelope_extractor.apply)
-        if self.method == 'geometric_correction':
-            mne_forward_model_file_path = self.traverse_back_and_find(
-                'mne_forward_model_file_path')
-            fwd = mne.read_forward_solution(
-                mne_forward_model_file_path, verbose='ERROR')
-            gain_matrix = fwd['sol']['data']
-            # Prepare topographies
-            if self.seed is None:
-                # Do all-to-all
-
-                # Get first singular vectors for each orientation inside label
-                g_xyz = [gain_matrix[:, ::3],
-                         gain_matrix[:, 1::3],
-                         gain_matrix[:, 2::3]]
-
-                labels = self.traverse_back_and_find('labels')
-                self.roi_forwards = []
-                for label in labels:
-                    g_xyz_label = [g[:, label.forward_vertices] for g in g_xyz]
-                    g_svd_label = [svd(g)[0][:, 0] for g in g_xyz_label]
-                    self.roi_forwards.append(svd(
-                        np.c_[g_svd_label[0], g_svd_label[1], g_svd_label[2]]
-                    )[0][:, :3])
-
-            else:
-                # Get seed topographies
-                fwd_seed_slice = slice(self.seed * 3, (self.seed + 1) * 3)
-                self.roi_forwards = [
-                    svd(gain_matrix[:, fwd_seed_slice])[0][:, :3]]
 
     def _update(self):
         input_data = self.input_node.output
@@ -1060,8 +1031,7 @@ class AmplitudeEnvelopeCorrelations(ProcessorNode):
         ddof = 1
         # ddof=1 is for unbiased std estimator
         envs = envs / envs.std(axis=1, ddof=ddof)[:, np.newaxis]
-        if self.method == 'temporal_orthogonalization':
-            gramm = data.dot(data.T)  # Gramm matrix
+        G = data.dot(data.T)  # Gramm matrix
 
         if self.seed is None:
             labels_iter = range(data.shape[0])
@@ -1069,12 +1039,7 @@ class AmplitudeEnvelopeCorrelations(ProcessorNode):
             labels_iter = [self.seed]
 
         for i, r in enumerate(labels_iter):
-            if self.method == 'temporal_orthogonalization':
-                data_orth_r = (data -
-                               np.outer(gramm[:, r], data[r, :]) / gramm[r, r])
-            elif self.method == 'geometric_correction':
-                data_orth_r = data - self.roi_forwards[i].dot(
-                    self.roi_forwards[i].T.dot(data))
+            data_orth_r = data - np.outer(G[:, r], data[r, :]) / G[r, r]
             orth_envs = self._envelope_extractor.apply(np.abs(data_orth_r))
             orth_envs -= orth_envs.mean(axis=1)[:, np.newaxis]
             orth_envs /= orth_envs.std(axis=1, ddof=ddof)[:, np.newaxis]
@@ -1083,8 +1048,7 @@ class AmplitudeEnvelopeCorrelations(ProcessorNode):
         if self.seed is None:
             return (corrmat + corrmat.T) / 2
         else:
-            # return corrmat[:, np.newaxis]
-            return corrmat
+            return corrmat[:, np.newaxis]
 
 
 class Coherence(ProcessorNode):
