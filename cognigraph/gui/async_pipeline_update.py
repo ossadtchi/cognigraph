082cabf50945841f0edc11628858d6080f0fe8fc
dmalt <dm.altukhov@ya.ru>
2019-03-05 18:50:30 +0300

    move async pipeline execution mechanics to a separate file

---
+++ cognigraph/gui/async_pipeline_update.py
@@ -0,0 +1,58 @@
+from PyQt5 import QtCore, QtWidgets
+import logging
+import time
+
+class Communicate(QtCore.QObject):
+    """Pyqt signals sender"""
+    sync_signal = QtCore.pyqtSignal()
+
+
+class AsyncUpdater(QtCore.QThread):
+    _stop_flag = False
+
+    def __init__(self, app, pipeline):
+        super(AsyncUpdater, self).__init__()
+        self.sender = Communicate()
+        self.sender.sync_signal.connect(
+            self.process_events_on_main_thread,
+            type=QtCore.Qt.BlockingQueuedConnection)
+        self.is_paused = True
+        self.app = app
+        self.logger = logging.getLogger(type(self).__name__)
+        self.pipeline = pipeline
+
+    def process_events_on_main_thread(self):
+        self.app.processEvents()
+
+    def run(self):
+        self._stop_flag = False
+        self.logger.info('Start pipeline')
+
+        is_first_iter = True
+        while True:
+            # start = time.time()
+            self.pipeline.update_all_nodes()
+            # end = time.time()
+            if is_first_iter:
+                # without this hack widgets are not updated unless
+                # you click on them
+                time.sleep(0.05)
+                is_first_iter = False
+
+            self.sender.sync_signal.emit()
+            if self._stop_flag is True:
+                QtWidgets.QApplication.processEvents()
+                break
+
+    def stop(self):
+        self.logger.info('Stop pipeline')
+        self._stop_flag = True
+
+    def toggle(self):
+        if self.is_paused:
+            self.is_paused = False
+            self.start()
+        else:
+            self.is_paused = True
+            self.stop()
+            self.wait(1000)
