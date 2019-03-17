8448a689c1d92919eea67a34cd1985d596e10f02
dmalt <dm.altukhov@ya.ru>
2019-03-17 15:41:51 +0300

    remove pipeline_editor module

--- cognigraph/gui/pipeline_editor.py
+++
@@ -1,9 +0,0 @@
-"""Pipeline editor for cognigraph"""
-
-if __name__ == '__main__':
-    from PyQt5.QtWidgets import QMainApplication
-    import sys
-    app = QApplication(sys.argv)
-    dialog = PipelineEditorDialog()
-    dialog.show()
-    sys.exit(app.exec_())
