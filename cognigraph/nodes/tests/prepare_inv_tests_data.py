3f68e8ffc7cfb94902da41f3ec989bb6e5f72bf9
dmalt <dm.altukhov@ya.ru>
2019-03-18 14:24:27 +0300

    add docstring

--- cognigraph/nodes/tests/prepare_inv_tests_data.py
+++ cognigraph/nodes/tests/prepare_inv_tests_data.py
@@ -9,6 +9,7 @@ test_data_path = op.join(COGNIGRAPH_ROOT, 'tests/data')
 
 @pytest.fixture
 def info(scope='session'):
+    """Get info with applied average projection"""
     info_src_path = op.join(test_data_path, 'Koleno_raw.fif')
     raw = Raw(info_src_path, preload=True)
     raw.set_eeg_reference('average', projection=True)
