83d582fe41fac551726a5a8b244605a7fc8cb4fe
dmalt <dm.altukhov@ya.ru>
2019-04-01 00:44:56 +0300

    Get files from DataDownloader

--- cognigraph/nodes/tests/prepare_inv_tests_data.py
+++ cognigraph/nodes/tests/prepare_inv_tests_data.py
@@ -3,6 +3,7 @@ import os.path as op
 from mne.io import Raw
 
 from cognigraph import COGNIGRAPH_ROOT
+from cognigraph.utils.io import DataDownloader
 
 test_data_path = op.join(COGNIGRAPH_ROOT, 'tests/data')
 
@@ -10,7 +11,8 @@ test_data_path = op.join(COGNIGRAPH_ROOT, 'tests/data')
 @pytest.fixture
 def info(scope='session'):
     """Get info with applied average projection"""
-    info_src_path = op.join(test_data_path, 'Koleno_raw.fif')
+    dloader = DataDownloader()
+    info_src_path = dloader.get_file('Koleno_raw.fif')
     raw = Raw(info_src_path, preload=True)
     raw.set_eeg_reference('average', projection=True)
     return raw.info
